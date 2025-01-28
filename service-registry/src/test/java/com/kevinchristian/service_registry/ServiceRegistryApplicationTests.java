37d21cc34a8f0b6d97bd3876a163b798c42d38ad
Kevin Christian <kdotchrist30@gmail.com>
2025-01-28 07:19:00 +0700

    feat: add service registry clients and load balancing

---
+++ service-registry/src/test/java/com/kevinchristian/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinchristian.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
